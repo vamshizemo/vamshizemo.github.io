e4b6a41a0cb63ae10e1b39cdd09e3b828a840bcc
vamshiraghav <vamshiraghava@gmail.com>
2020-05-22 20:08:13 +0530

    JS Assignment 2 completed

--- JsAssign2/script.js
+++ JsAssign2/script.js
@@ -1,7 +1,6 @@
 class Table {
     constructor(number) {
         this.number = number;
-        //this.items = new Array();
         this.items = new Map();
         this.itemsCount = 0;
     }
@@ -10,28 +9,21 @@ class Table {
         let total = 0;
         this.itemsCount = 0;
         for (let item of this.items) {
-            //console.log("inside");
             total += menu[item[0]].price * item[1];
             this.itemsCount += item[1];
-            //total += menu.price;
         }
         return total;
     }
+    //updates the bill for a cetain table
     getBill() {
         this.getTotal();
-        //console.log(this.number);
         var bill = document.getElementById("billdetails");
-        var msg = '<h1> Table no ' + this.number + ' - Bill Details</H1>';
-        //console.log(msg);
+        var msg = '<h1> Table no ' + this.number + ' - Order Details</H1>';
         console.log(this.itemsCount);
         if (this.itemsCount > 0) {
-            //console.log("enter in loop");
             msg += '<table id="' + this.number + '"><thead> <td>S.No</td> <td>Item Description </td> <td> Quantity</td> <td> Price(Rs) </td><td></td> </thead>';
             var count = 1;
-            //console.log(msg);
             for (let item of this.items) {
-                //total += menu[item[0]].price*item[1];
-                //total += menu.price;
                 msg += '<tr><td>' + count++ + '</td><td>' + menu[item[0]].description + '</td><td><input min="1" id="' + item[0] + '" onchange="changeBill(this)" type="number" value="' + item[1] + '"/></td><td>' + menu[item[0]].price * item[1] + '</td><td><img id="' + item[0] + '" onclick="deleteItem(this)" src="del.png" /></td> </tr>';
 
             }
@@ -56,14 +48,16 @@ class Item {
 
 var tables = new Array();
 var menu = new Array();
-tables.push(new Table(0));
-tables.push(new Table(1));
-tables.push(new Table(2));
+var numberOfTables=5;
+for(let i=0;i<=numberOfTables;i++){
+    tables.push(new Table(i));
+}
 
 function displayTables(val) {
     var tableList = document.getElementById("tableList");
     tableList.innerHTML = '';
     for (let table of tables) {
+        if(table.number==0){continue;}
         let tableElement = document.createElement("div");
         tableElement.setAttribute("class", "table");
         tableElement.setAttribute("id", "tb" + table.number);
@@ -77,21 +71,23 @@ function displayTables(val) {
         }
     }
 }
-menu.push(new Item(0, "chicken Biryani", 200));
+menu.push(new Item(0, "Chicken Biryani", 200));
 menu.push(new Item(1, "veg Biryani", 180));
 menu.push(new Item(2, "Veg pulav", 200));
 menu.push(new Item(3, "Fried Rice", 100));
 menu.push(new Item(4, "Ice Cream", 70));
-menu.push(new Item(0, "chicken Biryani", 200));
-menu.push(new Item(1, "veg Biryani", 180));
-menu.push(new Item(2, "Veg pulav", 200));
-menu.push(new Item(3, "Fried Rice", 100));
-menu.push(new Item(4, "Ice Cream", 70));
-menu.push(new Item(0, "chicken Biryani", 200));
-menu.push(new Item(1, "veg Biryani", 180));
-menu.push(new Item(2, "Veg pulav", 200));
-menu.push(new Item(3, "Fried Rice", 100));
-menu.push(new Item(4, "Ice Cream", 70));
+menu.push(new Item(5, "Paneer Butter Masala", 230));
+menu.push(new Item(6, "Veg Malai Kofta", 270));
+menu.push(new Item(7, "Naan", 30));
+menu.push(new Item(8, "Plain Rice", 70));
+menu.push(new Item(9, "Chiken Curry", 250));
+menu.push(new Item(10, "Chicken 65", 220));
+menu.push(new Item(11, "Mutton Biryani", 240));
+menu.push(new Item(12, "Sprite 500ml", 50));
+menu.push(new Item(13, "Vanilla Ice Cream", 90));
+menu.push(new Item(14, "Butter Scotch Ice Cream", 70));
+menu.push(new Item(15, "Chicken Tikka", 260));
+menu.push(new Item(16, "Plain Roti", 25));
 
 
 function displayMenus(val) {
@@ -99,7 +95,7 @@ function displayMenus(val) {
     menuList.innerHTML = '';
     for (let item of menu) {
         if (val == null || item.description.match(eval("/" + val + "/gi"))) {
-            menuList.innerHTML += '<div class="item" id="item' + item.id + '" draggable="true" ondragstart="passData(event)" > <div class="title">' + item.description + '</div><div class="price">' + item.price + '</div> ';
+            menuList.innerHTML += '<div class="item" id="item' + item.id + '" draggable="true" ondragstart="passData(event)" > <div class="title">' + item.description + '</div><div class="price">Rs.' + item.price + '/-</div> ';
         }
     }
 
@@ -117,13 +113,12 @@ function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("menuId");
     var tableId = ev.target.id.substring(2);
-    //console.log(tableId);
     if (tables[tableId].items.has(data)) {
         tables[tableId].items.set(data, tables[tableId].items.get(data) + 1);
     } else {
         tables[tableId].items.set(data, 1);
     }
-    //console.log(tables[tableId].items);
+    tables[tableId].getBill();
     displayTables();
 }
 
@@ -136,11 +131,9 @@ function showbill(ev) {
     var bill = document.getElementById("bill");
     bill.style.display = "block";
     var tableId = ev.target.id.substring(2);
-    //console.log(tableId);
     tables[tableId].getBill();
 }
 function closeSession(id) {
-    //console.log("int the session");
     tables[id].items.clear();
     tables[id].getBill();
     displayTables();
@@ -160,12 +153,10 @@ function filterTable(ev) {
 }
 
 function changeBill(element) {
-    console.log("changeBill");
     var menuId = element.id;
     var val = element.value;
     var tableId = element.parentNode.parentNode.parentNode.parentNode.id;
-    console.log(menuId, val, tableId);
-
+    
     if (val > 0) {
         tables[tableId].items.set(menuId, parseInt(val));
         displayTables();
@@ -174,10 +165,8 @@ function changeBill(element) {
 
 }
 function deleteItem(element) {
-    console.log("deleteitem");
     var menuId = element.id;
     var tableId = element.parentNode.parentNode.parentNode.parentNode.id;
-    console.log(menuId, tableId);
     tables[tableId].items.delete(menuId);
     displayTables();
     tables[tableId].getBill();
